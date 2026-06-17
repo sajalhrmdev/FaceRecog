ae9f059a7c5388996817a6b2ad383a606736e18c
DebaprasadM <debaprasadmaiti2000@gmail.com>
2026-06-17 14:27:31 +0530

    small ver

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 from fastapi import FastAPI, UploadFile, File
-# from PIL import Image
-# import numpy as np
-# import insightface
+from PIL import Image
+import numpy as np
+import insightface
 
 app = FastAPI()
 
 # face_app = insightface.app.FaceAnalysis()
 # face_app.prepare(ctx_id=0)
+
 # face_app = insightface.app.FaceAnalysis(
 #     providers=["CPUExecutionProvider"]
 # )
 
 # face_app.prepare(ctx_id=-1)
 
+face_app = insightface.app.FaceAnalysis(
+    name="buffalo_s",
+    providers=["CPUExecutionProvider"]
+)
+face_app.prepare(ctx_id=-1)
 @app.get("/health")
 def health():
     return {"success": True}
@@ -35,27 +41,27 @@ def health():
 #         "embedding": faces[0].embedding.tolist()
 #     }
 
-# @app.post("/embedding")
-# async def embedding(file: UploadFile = File(...)):
-#     try:
-#         image = Image.open(file.file).convert("RGB")
-#         image = np.array(image)
+@app.post("/embedding")
+async def embedding(file: UploadFile = File(...)):
+    try:
+        image = Image.open(file.file).convert("RGB")
+        image = np.array(image)
 
-#         faces = face_app.get(image)
+        faces = face_app.get(image)
 
-#         if not faces:
-#             return {
-#                 "success": False,
-#                 "message": "No face detected"
-#             }
+        if not faces:
+            return {
+                "success": False,
+                "message": "No face detected"
+            }
 
-#         return {
-#             "success": True,
-#             "embedding": faces[0].embedding.tolist()
-#         }
+        return {
+            "success": True,
+            "embedding": faces[0].embedding.tolist()
+        }
 
-#     except Exception:
-#         return {
-#             "success": False,
-#             "message": "Invalid image file"
-#         }
+    except Exception:
+        return {
+            "success": False,
+            "message": "Invalid image file"
+        }
