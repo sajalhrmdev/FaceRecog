7698efbf3bf06201ac94c4a6e15f3eaeb81d68bc
DebaprasadM <debaprasadmaiti2000@gmail.com>
2026-06-17 14:13:33 +0530

    some issue fixed

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ import insightface
 
 app = FastAPI()
 
-face_app = insightface.app.FaceAnalysis()
+# face_app = insightface.app.FaceAnalysis()
 # face_app.prepare(ctx_id=0)
+face_app = insightface.app.FaceAnalysis(
+    providers=["CPUExecutionProvider"]
+)
+
 face_app.prepare(ctx_id=-1)
 
 @app.get("/health")
